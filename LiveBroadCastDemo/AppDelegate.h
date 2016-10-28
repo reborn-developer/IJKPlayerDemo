f3746de7f28b80bb4ca022ae6bf56f85b8977338
reborn <343633674@qq.com>
2016-10-28 14:46:08 +0800

    Initial Commit

---
+++ LiveBroadCastDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiveBroadCastDemo
+//
+//  Created by 马英 on 16/10/28.
+//  Copyright © 2016年 reborn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LiveBroadCastDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiveBroadCastDemo
+//
+//  Created by 马英 on 16/10/28.
+//  Copyright © 2016年 reborn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
